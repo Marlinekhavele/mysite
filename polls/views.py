e22e0112b2114248fff46123b25ad2359202af61
MarlineKhavele <marlinekhavele6@gmail.com>
2020-01-28 20:18:44 +0300

    Added forms.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 
 
 def index(response, id):
@@ -10,4 +11,17 @@ def index(response, id):
 
 def home(response):
     return render(response, "polls/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "polls/create.html", {"form": form})
 
